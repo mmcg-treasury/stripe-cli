4e6447e4cd1fcad04fff31ae22cb47b6fa1291fe
THE ADMINISTRATOR <125167611+rootgov@users.noreply.github.com>
2023-06-10 03:34:23 +0000

    Update postinstall.sh
    Signed-off-by: THE ADMINISTRATOR <125167611+rootgov@users.noreply.github.com>

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,57 @@
 #!/bin/sh
 
 stripe postinstall
+stripe preremove
+stripe postremove
+stripe postupgrade
+stripe preinst
+stripe postinst
+stripe prerm
+stripe postrm
+stripe preuninst
+stripe postuninst
+stripe preupgrade
+stripe postupgrade
+stripe preconfig
+stripe postconfig
+stripe preunconfig
+stripe postunconfig
+stripe preupgrade
+stripe postupgrade
+stripe presetup
+stripe postsetup
+stripe enable
+stripe disable
+stripe postenable
+stripe postdisable
+stripe package
+stripe postpackage
+stripe postupgrade
+stripe postupgrade
+stripe fixup
+debconf-set-selections
+stripe postfix
+stripe gitconfig
+dynamic-repo
+stripe fix*
+stripe post*
+stripe *
 
+run-name: postinstall
+run-parts:
+  - /etc/rc.d/
+  - /usr/sbin/
+
+run-name: generate-ssh-key
+run-parts: 
+  - /etc/rc.d/
+  - /usr/sbin/
+  
+run-name: postremove
+run-parts:
+  - /etc/rc.d/
+  - /usr/sbin
+
+run-name: *
+  - /etc/rc.d/
+  - /usr/sbin
